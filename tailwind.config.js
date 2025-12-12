7cd0f5d20698fbafdbbee8a0f0f264c465d13307
Kirillzur <kirillzurav@gmail.com>
2025-12-12 22:46:03 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: "320px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1480px",
+    },
+    extend: {
+      colors: {
+        primary: "#027E6F",
+        secondary: "#EAEEF2",
+        stroke: "#6D758D",
+        text: "#0E101A",
+        light_gray: "#6A7282",
+        background: "#FFF",
+        background_secondary: "#FCFCF5",
+      },
+      fontFamily: {
+       primary: ["Inter", "system-ui", "sans-serif"],
+       secondary: ["Source Sans 3", "system-ui", "sans-serif"],
+      },
+      spacing: {
+        18: "4.5rem",
+        22: "5.5rem",
+        30: "7.5rem",
+      },
+      borderRadius: {
+        xl: "1rem",
+        "2xl": "1.25rem",
+      },
+    },
+  },
+  plugins: [],
+};
